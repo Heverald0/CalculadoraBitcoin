dac0f4a12866aac331bd91b148bb2a664fac648d
Heveraldo Augusto Silva Viana Filho <Heveraldoa8@gmail.com>
2025-06-08 23:11:18 -0300

    Iniciando repositório

---
+++ app/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "br.com.livrokotlin.calculadora_de_bitcoin"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "br.com.livrokotlin.calculadora_de_bitcoin"
+        minSdk = 28
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation ("org.jetbrains.anko:anko-commons:0.10.8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation ("androidx.appcompat:appcompat:1.6.1")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+
+}
---
+++ app/src/main/java/br/com/livrokotlin/calculadora_de_bitcoin/MainActivity.kt
@@ -0,0 +1,85 @@
+package br.com.livrokotlin.calculadora_de_bitcoin
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.net.URL
+import java.text.NumberFormat
+import java.util.Locale
+
+class MainActivity : AppCompatActivity() {
+    private val API_URL = "https://www.mercadobitcoin.net/api/BTC/ticker/"
+    private var cotacaoBitcoin: Double = 0.0
+
+    private lateinit var btnCalcular: Button
+    private lateinit var txtValor: EditText
+    private lateinit var txtCotacao: TextView
+    private lateinit var txtQtdBitcoins: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Inicializando as views
+        btnCalcular = findViewById(R.id.btn_calcular)
+        txtValor = findViewById(R.id.txt_valor)
+        txtCotacao = findViewById(R.id.txt_cotacao)
+        txtQtdBitcoins = findViewById(R.id.txt_qtd_bitcoins)
+
+        buscarCotacao()
+
+        btnCalcular.setOnClickListener {
+            calcular()
+        }
+    }
+
+         fun buscarCotacao() {
+             GlobalScope.launch(Dispatchers.Main) {
+                 try {
+                     val resposta = withContext(Dispatchers.IO) {
+                         URL(API_URL).readText()
+                     }
+
+                     val json = Gson().fromJson(resposta, TickerResponse::class.java)
+                     cotacaoBitcoin = json.ticker.last
+
+                     val f = NumberFormat.getCurrencyInstance(Locale("pt", "BR"))
+                     val cotacaoFormatada = f.format(cotacaoBitcoin)
+
+                     txtCotacao.text = cotacaoFormatada
+                 } catch (e: Exception) {
+                     e.printStackTrace()
+                     txtCotacao.text = "Erro ao buscar cotação"
+                 }
+             }
+         }
+    
+        fun calcular() {
+            if (txtValor.text.isEmpty()) {
+                txtValor.error = "Preencha um valor"
+                return
+            }
+
+            try {
+                val valorDigitado = txtValor.text.toString()
+                    .replace(",", ".")
+                    .toDouble()
+
+                val resultado = if (cotacaoBitcoin > 0) valorDigitado / cotacaoBitcoin else 0.0
+                txtQtdBitcoins.text = "%.8f".format(resultado)
+            } catch (e: NumberFormatException) {
+                txtValor.error = "Valor inválido"
+            }
+        }
+}
+
+// Classes para o parse do JSON
+data class TickerResponse(val ticker: Ticker)
+data class Ticker(val last: Double)
